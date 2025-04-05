af7f29cf36a83e30e300c6cdf2ed3985854ad28a
Dawid Mackiewicz <dawid2004m@gmail.com>
2025-04-06 00:05:21 +0200

    implemetaton of Main menu

--- Photo&VideoConverter/ViewModel/MainMenuViewModel.cs
+++ Photo&VideoConverter/ViewModel/MainMenuViewModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Photo_VideoConverter.ViewModel
 {
-    internal class MainMenuViewModel
+    internal class MainMenuViewModel : ObservableObject
     {
+        RelayCommand SwitchToConvertSettingCommand { get;  }
+        RelayCommand SwitchToConvertSignleFileCommand { get; }
+
+        public MainMenuViewModel()
+        {
+            SwitchToConvertSettingCommand = new RelayCommand(SwitchToConvertSetting);
+            SwitchToConvertSignleFileCommand = new RelayCommand(SwitchToConvertSingleFile);
+        }
+
+        private void SwitchToConvertSetting()
+        {
+            // Logic to switch to ConvertSettingViewModel
+            //to do when start working on the ConvertSettingViewModel
+        }
+        private void SwitchToConvertSingleFile()
+        {
+            // Logic to switch to ConvertSingleFileViewModel
+            //to do when start working on the ConvertSingleFileViewModel
+        }
+
+
     }
 }
