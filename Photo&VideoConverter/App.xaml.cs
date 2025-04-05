349a93250d1c9bc9c4b631ae66e99c2737c91ca6
Dawid Mackiewicz <dawid2004m@gmail.com>
2025-04-05 23:01:03 +0200

    Add project files.

---
+++ Photo&VideoConverter/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Photo_VideoConverter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Photo&VideoConverter/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Photo_VideoConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _readDirectory = string.Empty;
+        private string _writeDirectory = string.Empty;
+
+        private ObservableCollection<string> _VideoFormatList = new ObservableCollection<string> { "mp4", "avi", "mov", "mkv", "flv" };
+        private ObservableCollection<string> _PhotoFormatList = new ObservableCollection<string> { "png", "jpg", "webp", "bmp", "gif" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
